79d06909b14bf41b746aee8fbb26b1c4d67b24c7
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2022-10-04 21:23:22 -0600

    INICIAL

---
+++ build/controllers/CustomerVars.js
@@ -0,0 +1,12 @@
+let GlobalToken = 'MERCADOSEFECTIVOS';
+let GlobalCodSucursal = '';
+
+let GlobalEmpnit = '1034261-3';
+let GlobalEmpNombre = 'LTJ COBAN';
+
+
+let GlobalVentaMinima = 79.99;
+
+let dataEmpresas = [ 
+    {codsucursal:"LTJ-COBAN",nomsucursal:"LTJ COBAN",color:"success"}
+  ];
